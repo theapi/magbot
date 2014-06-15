de2bc1d348819200d1b544c7b63678ee97b3aa31
theapi <petermclarke@gmail.com>
2014-06-15 09:46:46 +0100

    Check battery level

--- magbot_test/magbot_test.ino
+++ magbot_test/magbot_test.ino
@@ -4,6 +4,7 @@
  speaker/piezo for sound
  */
 
+#include <util/delay.h>
 #include <NewPing.h> // From https://code.google.com/p/arduino-new-ping/
 #include <NewTone.h> // From https://code.google.com/p/arduino-new-tone/
 
@@ -81,6 +82,11 @@ void loop() {
     // Remeber when this ping was done, so we know when to do the next one.
     melody_last = millis_now;
     playMelody();
+    
+    // Check battery level
+    long vcc = readVcc();
+    Serial.print("Battery: ");
+    Serial.println(vcc);
   }
   
 }
@@ -102,5 +108,35 @@ void playMelody() {
     // stop the tone playing:
     noNewTone(tone_pin);
   }
+}
+
+/**
+ * Read the internal voltage.
+ */
+long readVcc() 
+{
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+ 
+  _delay_ms(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+ 
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+ 
+  long result = (high<<8) | low;
+ 
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
 }
 
